7e2b397f5701aacf5f7367a593d682e74ff60d0e
Melvin Klein <70433111+MelvinKl@users.noreply.github.com>
2025-07-04 23:12:25 +0200

    revert

--- admin-backend/main.py
+++ admin-backend/main.py
@@ -1,6 +1,2 @@
-from admin_api_lib.main import app as perfect_admin_app, register_dependency_container  # noqa: F401
+from admin_api_lib.main import app as perfect_admin_app  # noqa: F401
 
-from container import UseCaseContainer
-
-
-register_dependency_container(UseCaseContainer())
