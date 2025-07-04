305e2dd2f8c34f366f8f9bb2c20464b1d8f44b8d
Melvin Klein <70433111+MelvinKl@users.noreply.github.com>
2025-07-04 22:01:09 +0200

    Create container.py

---
+++ admin-backen/container.py
@@ -0,0 +1,20 @@
+"""
+This is an example of how to replace a dependency in the dependency container of the rag-core-api library.
+If you replace a dependency keep in mind that the dependency you are replacing should have:
+1. the same name and
+2. the same base class
+as the dependency you are replacing it with.
+
+Furthermore, if the dependency you are replacing has any dependencies from the dependency container,
+they can be simply reused in the new dependency as shown in the example.
+"""
+from langchain_openai import ChatOpenAI
+from dependency_injector.providers import Singleton
+from dependency_injector import containers
+
+from rag_core_api.dependency_container import DependencyContainer
+
+
+@containers.copy(DependencyContainer)
+class UseCaseContainer(DependencyContainer):
+    large_language_model = Singleton(DependencyContainer.llm_provider, DependencyContainer.stackit_vllm_settings, ChatOpenAI)
