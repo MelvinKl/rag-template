5bb97bf7ad304b9ffc3eefd50406307e69ced50b
Melvin Klein <70433111+MelvinKl@users.noreply.github.com>
2025-07-05 00:23:09 +0200

    switch to ChatOpenAI

---
+++ admin-backend/container.py
@@ -0,0 +1,63 @@
+
+"""
+
+
+This is an example of how to replace a dependency in the dependency container of the rag-core-api library.
+
+
+If you replace a dependency keep in mind that the dependency you are replacing should have:
+
+
+1. the same name and
+
+
+2. the same base class
+
+
+as the dependency you are replacing it with.
+
+
+
+
+
+Furthermore, if the dependency you are replacing has any dependencies from the dependency container,
+
+
+they can be simply reused in the new dependency as shown in the example.
+
+
+"""
+
+
+from langchain_openai import ChatOpenAI
+
+
+from dependency_injector.providers import Singleton
+
+
+from dependency_injector import containers
+
+
+
+
+
+from admin_api_lib.dependency_container import DependencyContainer
+
+
+from rag_core_lib.impl.llms.llm_factory import llm_provider
+from rag_core_lib.impl.utils.async_threadsafe_semaphore import AsyncThreadsafeSemaphore
+from summarizer import CustomLangchainSummarizer
+
+
+@containers.copy(DependencyContainer)
+class UseCaseContainer(DependencyContainer):
+
+
+    large_language_model = Singleton(llm_provider, DependencyContainer.stackit_vllm_settings, ChatOpenAI)
+
+    summarizer = Singleton(
+        CustomLangchainSummarizer,
+        langfuse_manager=DependencyContainer.langfuse_manager,
+        chunker=DependencyContainer.summary_text_splitter,
+        semaphore=Singleton(AsyncThreadsafeSemaphore, DependencyContainer.summarizer_settings.maximum_concurrreny),
+    )
--- admin-backend/main.py
+++ admin-backend/main.py
@@ -1,2 +1,16 @@
-from admin_api_lib.main import app as perfect_admin_app  # noqa: F401
 
+from admin_api_lib.main import app as perfect_admin_app, register_dependency_container  # noqa: F401
+
+
+
+
+from container import UseCaseContainer
+
+
+
+
+
+
+
+
+register_dependency_container(UseCaseContainer())
